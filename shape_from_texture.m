c8b313ee1de908b755228034c9dafbc9f0dd5549
Alice Pigneux <75853361+apigneux@users.noreply.github.com>
2022-01-22 22:31:37 +0100

    Add files via upload

---
+++ shape_from_texture.m
@@ -0,0 +1,53 @@
+clear;
+close all;
+
+% Load ellipses.txt
+ellipses = fopen('ellipses.txt','r');
+
+parametres_ell = fscanf(ellipses,'%f', [7 Inf]);
+parametres_ell = parametres_ell';
+parametres_ell(1,:)
+fclose(ellipses);
+
+% Filtrage des ellipses ayant un angle < 4.7 (radian)
+angle = abs(parametres_ell(:,7)-parametres_ell(:,6));
+[indices] = find(angle > 4.7);
+angle = angle(indices);
+
+figure(1);
+scatter((1:length(angle))',angle');
+
+% Calcul excentricité des ellipses
+parametres_ell = parametres_ell(indices);
+e = [];
+% Grand-axe
+a = parametres_ell(:,3);
+% Petit-axe
+b = parametres_ell(:,4);
+
+e = sqrt(1-(b./a).^2);
+
+figure(2);
+scatter(parametres_ell(:,2), parametres_ell(:,1));
+ylim([0 2654]);
+xlim([0 1998]);
+
+% Calcul des normales
+R = 2;
+psi = parametres_ell(:,5);
+
+nz = b./R;
+nx = sqrt(1-nz.^2)./sqrt(1+tan(psi).^2);
+ny = tan(psi).*(sqrt(1-nz.^2)./(1+tan(psi).^2));
+
+% Intégration du champ de normales :
+
+p_estime = reshape(-nx'./nz,size(masque));
+p_estime(exterieur) = 0;
+q_estime = reshape(-ny'./nz',size(masque));
+q_estime(exterieur) = 0;
+z_estime = integration_SCS(q_estime,p_estime);
+
+
+
+
